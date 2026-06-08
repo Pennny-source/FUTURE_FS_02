a204f9b967d0d533159f7ad3f31d9608f26a0442
Pennny-source <phenyophooko@gmail.com>
2026-06-08 17:16:12 +0200

    Update frontend API URL

--- client/src/services/leadService.js
+++ client/src/services/leadService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/leads' });
+const API = axios.create({ baseURL: 'http://import.meta.env.VITE_API_URL/api/leads' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'http://import.meta.env.VITE_API_URL/api/tasks';
 
 export const taskService = {
   getAll: () =>
